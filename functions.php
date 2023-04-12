6d51fa28e3894f4fe1140687cd0c1d3b34b3c6c6
pjehan <pierre.jehan@gmail.com>
2023-04-12 12:28:45 +0200

    Création du header et du footer du site

--- functions.php
+++ functions.php
@@ -4,3 +4,8 @@ add_action('wp_enqueue_scripts', 'portfolio_enqueue_styles');
 function portfolio_enqueue_styles() {
     wp_enqueue_style('theme-style', get_template_directory_uri() . '/style.css', [], wp_get_theme()->get('Version'));
 }
+
+add_action('after_setup_theme', 'portfolio_add_theme_support');
+function portfolio_add_theme_support() {
+    add_theme_support('title-tag');
+}
