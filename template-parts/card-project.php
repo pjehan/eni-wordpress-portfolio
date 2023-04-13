27947101b21350c59eab12ea16f4a2239eb65abb
pjehan <pierre.jehan@gmail.com>
2023-04-13 14:40:20 +0200

    Correction d'un bug sur la liste des projets si un projet n'a pas de technologies associées

--- template-parts/card-project.php
+++ template-parts/card-project.php
@@ -6,11 +6,14 @@
         <h2><?php the_title() ?></h2>
 
         <?php //the_taxonomies(); ?>
-        <?php foreach (get_the_terms(get_the_ID(), 'technology') as $technology) : ?>
-            <a href="<?= get_term_link($technology->term_id); ?>" class="btn btn-secondary btn-small">
-                <?= $technology->name ?>
-            </a>
-        <?php endforeach; ?>
+        <?php $technologies = get_the_terms(get_the_ID(), 'technology'); ?>
+        <?php if (is_array($technologies)): ?>
+            <?php foreach ($technologies as $technology) : ?>
+                <a href="<?= get_term_link($technology->term_id); ?>" class="btn btn-secondary btn-small">
+                    <?= $technology->name ?>
+                </a>
+            <?php endforeach; ?>
+        <?php endif; ?>
 
         <p><?php the_excerpt() ?></p>
     </main>
